8e33bf1bf12ecc0df71ca2f2d951c5c82d8f82fe
Florian B <floryn93@gmail.com>
2017-11-20 22:55:57 +0200

    Add Home component.

---
+++ src/components/Home.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="row">
+      <h1 class="text-center">{{ message }}</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      message: 'Welcome Home!'
+    };
+  }
+};
+</script>
+
+<style>
+
+</style>
