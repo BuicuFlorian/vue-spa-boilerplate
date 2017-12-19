f57f95e117cb66ad72c62ab8a519472f625cbe65
Florian B <floryn93@gmail.com>
2017-12-19 22:52:09 +0200

    Fix missing semicolon issue.

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const path = require('path');
 const app = express();
 const port = process.env.PORT || 8080;
 
-app.use(express.static(path.join(__dirname, './build'))
+app.use(express.static(path.join(__dirname, './build'));
 
 app.get('*', (req, res) => {
     res.sendFile(path.join(__dirname, './dist/index.html'));
