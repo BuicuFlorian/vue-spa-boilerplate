90289e91d53402ebd91995eed2fdba9bdc9a032b
Florian B <florian.buicu@yahoo.com>
2024-03-07 20:54:56 +0200

    Add page not found view.

---
+++ src/views/PageNotFound.vue
@@ -0,0 +1,14 @@
+<script lang="ts" setup>
+import { useRouter } from 'vue-router';
+
+const router = useRouter();
+</script>
+
+<template lang="pug">
+.container
+  .row.text-center
+    .col-12.pt-5
+      h2 Page Not Found
+
+    a(href="#" @click.prevent="router.back()") Go back
+</template>
