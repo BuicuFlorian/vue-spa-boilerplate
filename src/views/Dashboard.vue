2abf546ee94090d616f4893ae2d499b79a3eefcd
Florian B <florian.buicu@yahoo.com>
2024-03-07 20:57:15 +0200

    Rewrite the dashboard using Vue 3 and TypeScript.

---
+++ src/views/Dashboard.vue
@@ -0,0 +1,23 @@
+<script lang="ts" setup>
+import { useFetch } from '@vueuse/core'
+
+const { isFetching, error, data } = useFetch('/api/users')
+</script>
+
+<template lang="pug">
+.container 
+  .row 
+    .col-12.text-center.mt-5
+      h1 Dashboard
+
+      .error-message(v-if='error') {{ error }}
+      .spinner(v-if='isFetching')
+        i.fa.fa-spinner.fa-pulse
+      pre(v-else) {{ data }}
+
+</template>
+
+<style lang="stylus">
+.error-message
+  color red
+</style>
